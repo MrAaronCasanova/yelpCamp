c0d8239a1f46a35773f53433252c2116ed7ebeb2
MrAaronCasanova <1boris231@gmail.com>
2018-01-18 11:14:10 -0800

    configured, refactored, and styled show page

--- v5/seeds.js
+++ v5/seeds.js
@@ -6,17 +6,17 @@ var data = [
   {
     name: 'Clouds Rest',
     image: 'https://images.pexels.com/photos/795722/pexels-photo-795722.jpeg?h=350&auto=compress&cs=tinysrgb',
-    description: 'blah blah blah',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis minus repudiandae fugit, cumque possimus pariatur ipsa, vitae totam amet optio tenetur magnam fuga temporibus. Ducimus asperiores optio delectus, eaque voluptatum, voluptate amet accusamus culpa similique nihil nemo, totam consequuntur labore cupiditate reiciendis voluptates aliquid.',
   },
   {
     name: 'Island Reserve',
     image: 'https://images.pexels.com/photos/787619/pexels-photo-787619.jpeg?h=350&auto=compress&cs=tinysrgb',
-    description: 'blah blah blah',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis minus repudiandae fugit, cumque possimus pariatur ipsa, vitae totam amet optio tenetur magnam fuga temporibus. Ducimus asperiores optio delectus, eaque voluptatum, voluptate amet accusamus culpa similique nihil nemo, totam consequuntur labore cupiditate reiciendis voluptates aliquid.',
   },
   {
     name: 'Winter Wonder',
     image: 'https://images.pexels.com/photos/15382/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb',
-    description: 'blah blah blah',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis minus repudiandae fugit, cumque possimus pariatur ipsa, vitae totam amet optio tenetur magnam fuga temporibus. Ducimus asperiores optio delectus, eaque voluptatum, voluptate amet accusamus culpa similique nihil nemo, totam consequuntur labore cupiditate reiciendis voluptates aliquid.',
   },
 ];
 
