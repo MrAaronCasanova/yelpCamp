af14b92ae39bd159d14673bd9a1100de8fe07cd3
MrAaronCasanova <1boris231@gmail.com>
2018-01-18 09:53:44 -0800

    comments are working, about to start styling

--- v4/app.js
+++ v4/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
+var Comment = require('./models/comment');
 var seedDB = require('./seeds');
 
 mongoose.connect('mongodb://localhost/yelp_camp_v3', { useMongoClient: true });
@@ -84,6 +85,30 @@ app.get('/campgrounds/:id/comments/new', function (req, res) {
       res.render('comments/new', { campground: campground });
     }
   });
+});
+
+app.post('/campgrounds/:id/comments', function (req, res) {
+  // Lookup campground using ID
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, function (err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      });
+    }
+  });
+
+  // Create new comments
+  // Connect new comment to campgrounds
+  // Redirect to campground show page
 });
 
 app.listen(3000, function () {
